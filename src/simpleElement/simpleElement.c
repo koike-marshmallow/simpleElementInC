9f97b5297fbb4ba75788d94e8357e3742ed1c82c
taka.k <taka@localhost.localdomain>
2016-01-04 13:30:45 +0900

    simpleElement作成中

--- src/simpleElement/simpleElement.c
+++ src/simpleElement/simpleElement.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include "nodeCore.h"
 
+#define BUF_SHORT_LEN 32
+#define BUF_LONG_LEN 256
+
 /*
 NODE* createElement(char* name);
 NODE* createTextNode(char* text);
@@ -54,13 +57,114 @@ void recursiveDestroyElement(NODE* element){
 
 void initElement(NODE* element){
 	int type = getNodeType(element);
+	
+	initNode(element, type);
+}
 
+
+void clearElement(NODE* element){
+	clearNode(element);
+}
+
+
+void addAttribute(NODE* element, char* name, char* value){
+	NODE* new_attr;
 	
+	new_attr = createNode(NODE_ATTRIBUTE);
+	setNodeName(new_attr, name);
+	setNodeValue(new_attr, value);
 	
+	appendChildNode(new_attr);
+}
+
+
+void deleteAttribute(NODE* element, char* name){
+	int child_len, i;
+	char buffer[BUF_SHORT_LEN];
+	NODE* child;
 	
+	child_len = getChildNodeCount(element);
+	for( i=(child_len - 1); i>=0; i--){
+		child = getChildNode(element, i);
+		if( getNodeType(child) == NODE_ATTRIBUTE ){
+			getNodeName(child, buffer, BUF_SHORT_LEN);
+			
+			if( strcmp(buffer, name) == 0 ){
+				removeChildNode(element, i);
+			}
+		}
+	}
+}
+
+
+void clearAttribute(NODE* element){
+	int child_len, i;
+	NODE* child;
 	
+	child_len = getChildNodeCount(element);
+	for( i=(child_len - 1); i>=0; i--){
+		child = getChildNode(element, i);
+		if( getNodeType(child) == NODE_ATTRIBUTE ){
+			removeChildNode(element, i);
+		}
+	}
+}
+
+
+void getAttributeNameList(NODE* element, char* list[], int l_size, int b_size){
+	int child_len, i, cnt;
+	NODE* child;
 	
+	child_len = getChildNodeCount(element);
+	cnt = 0;
+	for( i=0; (i < child_len) && (cnt < l_size); i++){
+		child = getChildNode(element, i);
+		if( getNodeType(child) == NODE_ATTRIBUTE ){
+			getNodeName(child, list[cnt], b_len);
+			cnt++;
+		}
+	}
+}
+
+
+void getAttributeValue(NODE* element, char* name, char buf[], int len){
+	int child_len, i;
+	char buffer[BUF_SHORT_LEN];
+	NODE* child;
 	
+	child_len = getChildNodeCount(element);
+	for( i=(child_len - 1); i>=0; i--){
+		child = getChildNode(element, i);
+		if( getNodeType(child) == NODE_ATTRIBUTE ){
+			getNodeName(child, buffer, BUF_SHORT_LEN);
+			
+			if( strcmp(buffer, name) == 0 ){
+				getNodeValue(child, buf, len);
+			}
+		}
+	}
+}
+		
+	
+void addText(NODE* element, char* text){
+	NODE* new_text;
+	
+	new_text = createNode(NODE_TEXT);
+	setNodeValue(text);
+	
+	appendChildNode(element, new_text);
+}
+
+
+void getText(NODE* element, char buf[], int len){
+	int child_len, i;
+	char buffer[BUF_LONG_LEN];
+	NODE* child;
+	
+	memset(buf, 0, len);
+	child_len = getChildNodeCount(element);
+	for( i=0; (i < child_len) && (strlen(buf) < len - 1); i++){
+		
 	
 	
 	
--- src/simpleElement/simpleElement.h
+++ src/simpleElement/simpleElement.h
@@ -9,10 +9,10 @@ void clearElement(NODE* element);
 void addAttribute(NODE* element, char* name, char* value);
 void deleteAttribute(NODE* element, char* name);
 void clearAttribute(NODE* element);
-int getAttributeNameList(NODE* element, char* list[], int len);
+int getAttributeNameList(NODE* element, char* list[], int l_size, int b_size);
 void getAttributeValue(NODE* element, char* name, char buf[], int len);
 
-void setText(NODE* element, char* text);
+void addText(NODE* element, char* text);
 void getText(NODE* element, char buf[], int len);
 void clearText(NODE* element);
 
