6f205a09af4a7496d1b16daeed1e06c8e6c11046
koike-marshmallow <taka.vao.321@gmail.com>
2016-01-24 10:38:45 +0900

    jsonNodeヘッダを編集

--- src/jsonNode/jsonNode.h
+++ src/jsonNode/jsonNode.h
@@ -17,28 +17,44 @@
 
 struct s_jsoncontent{
 	int dtype;
+	char* name;
 	void* value;
 }
 typedef struct s_jsoncontent JCONTENT;
 
-typedef int JSON_DT_INT;
-typedef double JSON_DT_FLOAT;
-typedef char JSON_DT_STRING;
-typedef int JSON_DT_BOOLEAN;
+typedef int JNODE_DT_INT;
+typedef double JNODE_DT_FLOAT;
+typedef char JNODE_DT_STRING;
+typedef int JNODE_DT_BOOLEAN;
 
 
 NODE* createJsonNode();
 void initJsonNode(NODE* jnode);
 void destroyJsonNode(NODE* jnode);
 
-void json_setInt(NODE* jnode, JSON_DT_INT val);
-void json_setFloat(NODE* jnode, JSON_DT_FLOAT val);
-void json_setString(NODE* jnode, char* str);
-void json_setBool(NODE* jnode, JSON_DT_BOOLEAN val);
+void jnode_setName(NODE* jnode char* str);
+char* jnode_getNmae(NODE* jnode);
 
-void json_setNull(NODE* jnode);
-void json_setObject(NODE* jnode, NODE* obj);
-void json_arrayAppendNode(NODE* jnode, NODE* apnd);
+void jnode_setInt(NODE* jnode, JNODE_DT_INT val);
+void jnode_setFloat(NODE* jnode, JNODE_DT_FLOAT val);
+void jnode_setString(NODE* jnode, char* str);
+void jnode_setBool(NODE* jnode, JNODE_DT_BOOLEAN val);
 
+void jnode_setNull(NODE* jnode);
+void jnode_setObject(NODE* jnode, NODE* obj);
+void jnode_setArray(NODE*: jnode, NODE* ary);
 
+int jnode_getType(NODE* jnode);
+JSON_DT_INT jnode_getInt(NODE* jnode);
+JSON_DT_FLOAT jnode_getFloat(NODE* jnode);
+char* jnode_getString(NODE* jnode);
+JSON_DT_BOOLEAN jnode_getBool(NODE* jnode);
 
+NODE* jnode_getObject(NODE* jnode);
+NODE* jnode_getArray(NODE* jnode);
+
+void jnode_insertChild(NODE* jnode, int idx, NODE* child);
+void jnode_appendChild(NODE* jnode, NODE* child);
+NODE* jnode_removeChild(NODE* jnode, int idx);
+
+void clearValue(NODE* jnode);
